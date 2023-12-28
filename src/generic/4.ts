eabb210679b9d3883d12a6e1e9ecf21d2d28efb8
OleksanderMelnik <alekandrmelnik@gmail.com>
2023-12-28 11:38:49 +0200

    generic 1-8

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,15 +1,18 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
+interface Book {
+  title: any;
+}
 
-class Component {
+class Component<T> {
   constructor (public props:T) {
 
   }
 }
 
-class Page extends Component {
-  pageInfo () {
+class Page extends Component<Book> {
+  pageInfo (): void {
     console.log(this.props.title);
   }
 }
